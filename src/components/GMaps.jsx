9dacbd8d8d24f7db140bea14ebe5e50b73985471
Kaai Suzuki <suzydp@me.com>
2018-12-04 21:47:08 -0800

    add test comment on GMaps.jsx

---
+++ src/components/GMaps.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+export default class GMaps extends Component{
+  render(){
+    return(
+      <div>
+        This component is for showing google maps
+      </div>
+    )
+  }
+}
