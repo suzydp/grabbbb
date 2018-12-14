81bf2e6d11595274b1012a3e41361699c7198370
Kaai Suzuki <suzydp@me.com>
2018-12-13 17:08:05 -0800

    Add SVG icons on TopPage.jsx

--- src/components/GMaps/GMaps.jsx
+++ src/components/GMaps/GMaps.jsx
@@ -13,11 +13,61 @@ export default class GMaps extends Component{
     zoom: 12,
   }
 
+//   const initMap = () => {
+//   const map = document.querySelector('.googlemaps');
+//   let target;
+
+//   // //enable geolocation API
+//   //1.check user-agent
+//   if (!navigator.geolocation) {
+//     alert('This browser is not supported geo location system.');
+//     return;
+//   }
+
+
+//   try {
+//     console.log('ちょっと待ってね'); // TODO くるくる見せる
+//     const userLocation = await getUserLocation();//awaitは普通Promiseとセットで使用(今回の場合は下にあるPromise) - awaitはPromiseの処理が終わるのを待つ
+//     //Promise処理が終わったら
+//     target = new google.maps.Map(map, {//targetに以下の情報を仕込む
+//       center: userLocation,
+//       zoom: 15,
+//     });
+//     console.log('いいよ'); // TODO くるくる消す
+//   } catch (error) {//tryの中で何かしらエラーが出た場合はすぐにcatchに飛んで来る(いいよは出ない)
+//     console.error(error);
+//   }
+
+//   //Google places API - API for searching places
+//   document.querySelector('#search').addEventListener('click', function () {
+//     console.log('clicked')
+//     const keyword = document.querySelector('#keyword').value;
+//     searchBy(target, keyword);//下のsearchBy()を呼び出す
+//   });
+// }
+
+//   //ユーザーの位置情報を拾って来る関数
+//   const getUserLocation = () => {
+//     return new Promise((resolve, reject) => {//Promiseを定義
+//       //Allowした場合(denyした場合はconsoleにエラーが表示)
+//       navigator.geolocation.getCurrentPosition((position) => {//positionにユーザーの位置情報が入ってくる
+//         //Promise処理が成功した場合の処理だけ定義
+//         // 位置情報を整形
+//         const location = {
+//           lat: position.coords.latitude,
+//           lng: position.coords.longitude
+//         };
+//         //処理が成功したら上記で定義した処理を走らせる(失敗したら何もしない)
+//         resolve(location);
+//       }, reject);//( (position) => {}というファンクション, reject )
+//     });
+//   }
+
   render(){
     return(
       <div style={{ height:'100vh', width:'100%' }}>
         Google map component
-        <GoogleMapReact
+        <GoogleMapReact className="googlemaps"
           // bootstrapURLKeys={{ key: /* your key here */ }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
--- src/components/TopPage/TopPage.jsx
+++ src/components/TopPage/TopPage.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from './../Footer/Footer.jsx';
+import CoffeeIcon from './../Icons/Coffee.js';
+import JuiceIcon from './../Icons/Juice.js';
+import WifiIcon from './../Icons/Wifi.js';
+import OutletIcon from './../Icons/Outlet.js';
 import './TopPage.scss';
 
 export default class TopPage extends Component{
@@ -13,10 +17,10 @@ export default class TopPage extends Component{
         </div>
         <div className="menus">
           <ul className="options">
-            <li><Link to="/coffee" className="option-circle coffee"></Link><span className="option-title">Coffee</span></li>
-            <li><Link to="/juice" className="option-circle juice"></Link><span className="option-title">Softdrinks</span></li>
-            <li><Link to="/wifi" className="option-circle wifi"></Link><span className="option-title">Wi-fi</span></li>
-            <li><Link to="/outlet" className="option-circle outlet"></Link><span className="option-title">Outlet</span></li>
+            <li><Link to="/coffee" className="option-circle coffee"><CoffeeIcon className="Coffee" width={100} /></Link><span className="option-title">Coffee</span></li>
+            <li><Link to="/juice" className="option-circle juice"><JuiceIcon className="Juice" width={100} /></Link><span className="option-title">Softdrinks</span></li>
+            <li><Link to="/wifi" className="option-circle wifi"><WifiIcon className="wifi" width={100} /></Link><span className="option-title">Wi-fi</span></li>
+            <li><Link to="/outlet" className="option-circle outlet"><OutletIcon className="Outlet" width={100} /></Link><span className="option-title">Outlet</span></li>
           </ul>
         </div>
         <Footer/>
