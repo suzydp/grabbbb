e005ceb45c74ec7027b480ef20df018ee1a22ae3
Kaai Suzuki <kaaisuzuki@Kaais-MacBook-Pro.local>
2018-12-16 21:26:41 -0800

    Add withScriptjs to render

--- src/components/GMaps/GMaps.jsx
+++ src/components/GMaps/GMaps.jsx
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 // {} - import submodules from the library
-import { GoogleMap, Marker } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import './GMaps.scss';
 
-const MyMapComponent = (props) => 
+const GMapsForGrabbbb = withScriptjs(withGoogleMap((props) => 
   <GoogleMap
-    defaultZoom={8}
+    defaultZoom={15}
     defaultCenter={{ lat: 40.7575285, lng: -73.9884469 }}
   >
     {props.isMarkerShown && <Marker position={{ lat: 40.7575285, lng: -73.9884469 }} />}
   </GoogleMap>
-
+))
 
 export default class GMaps extends Component{
   render() {
     return(
       <div>
-        Google Maps Component
-        <MyMapComponent isMarkerShown />
+        <GMapsForGrabbbb 
+          isMarkerShown 
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100vh`, width: `100vw`, }} />}//この二重括弧の意味って何？
+          containerElement={<div style={{ height: `100vh`, width: `100vw`, }} />}
+          mapElement={<div style={{ height: `100vh`, width: `100vw` }} />}
+        />
       </div>
     )
   }
