0d041222eaab5da961dc0972a7cb7bae0f7be309
Kaai Suzuki <suzydp@me.com>
2019-02-10 21:11:25 -0800

    Preparing for deploy

---
+++ build/precache-manifest.96e4558fba6fc1f1aca7942d717a00ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d5d79ca1839a8838862",
+    "url": "/static/css/main.df82ae9f.chunk.css"
+  },
+  {
+    "revision": "3d5d79ca1839a8838862",
+    "url": "/static/js/main.3d5d79ca.chunk.js"
+  },
+  {
+    "revision": "2fc8efaec3a8f52c344c",
+    "url": "/static/js/1.2fc8efae.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bd43e27babd9061c4f810d72dbe33d79",
+    "url": "/static/media/pin-coffee-mob.bd43e27b.svg"
+  },
+  {
+    "revision": "4278756c1bf236443485e1c6ec89e3f3",
+    "url": "/static/media/pin-bubbletea-mob.4278756c.svg"
+  },
+  {
+    "revision": "ffed7a39b98854ea3ac1092a8fe35dee",
+    "url": "/static/media/pin-wifi-mob.ffed7a39.svg"
+  },
+  {
+    "revision": "0db705e768869d74e71c9753c28b7cd0",
+    "url": "/static/media/pin-outlet-mob.0db705e7.svg"
+  },
+  {
+    "revision": "1b8c4c52fe8a27f8b6eb83b6d6a65967",
+    "url": "/index.html"
+  }
+];
