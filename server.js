006bd39b1e78224acdd189250ddf1aa409bdcd93
Kaai Suzuki <suzydp@me.com>
2019-03-20 14:24:09 -0700

    Fix favicon deployment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/build/favicon.png'));
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
