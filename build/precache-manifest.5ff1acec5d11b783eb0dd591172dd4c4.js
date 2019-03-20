cad481a9bbd0f4ec0f26c0c3d8e44da1a3e05b47
Kaai Suzuki <suzydp@me.com>
2019-03-20 14:04:50 -0700

    Replace favicon

--- build/precache-manifest.5ff1acec5d11b783eb0dd591172dd4c4.js
+++ build/precache-manifest.5ff1acec5d11b783eb0dd591172dd4c4.js
@@ -1,19 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "1d0b32512ff7c994bfcc",
-    "url": "/static/css/main.8010fc4e.chunk.css"
+    "revision": "2c6c751b3be001a3da9f",
+    "url": "/static/css/main.23e9f72a.chunk.css"
   },
   {
-    "revision": "1d0b32512ff7c994bfcc",
-    "url": "/static/js/main.1d0b3251.chunk.js"
+    "revision": "2c6c751b3be001a3da9f",
+    "url": "/static/js/main.2c6c751b.chunk.js"
   },
   {
-    "revision": "2fc8efaec3a8f52c344c",
-    "url": "/static/js/1.2fc8efae.chunk.js"
+    "revision": "e86e1977e1ca65919e6b",
+    "url": "/static/js/1.e86e1977.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
     "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ffed7a39b98854ea3ac1092a8fe35dee",
+    "url": "/static/media/pin-wifi-mob.ffed7a39.svg"
   },
   {
     "revision": "bd43e27babd9061c4f810d72dbe33d79",
@@ -24,15 +28,11 @@ self.__precacheManifest = [
     "url": "/static/media/pin-bubbletea-mob.4278756c.svg"
   },
   {
-    "revision": "ffed7a39b98854ea3ac1092a8fe35dee",
-    "url": "/static/media/pin-wifi-mob.ffed7a39.svg"
-  },
-  {
     "revision": "0db705e768869d74e71c9753c28b7cd0",
     "url": "/static/media/pin-outlet-mob.0db705e7.svg"
   },
   {
-    "revision": "16b6ef9f7ca66fc74f634baaea9b960f",
+    "revision": "ce210dc2e79d1fb8bd30beb0734248da",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.189bfddc34433e12794807e88efdd309.js"
+  "/precache-manifest.5ff1acec5d11b783eb0dd591172dd4c4.js"
 );
 
 workbox.clientsClaim();
